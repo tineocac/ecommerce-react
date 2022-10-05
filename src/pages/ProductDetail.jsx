b5cccebbc4700abbd6fa1862894a4b24a84efc1c
tineocac <carlostineocac@gmail.com>
2022-10-05 14:09:32 -0400

    related products

--- src/pages/ProductDetail.jsx
+++ src/pages/ProductDetail.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import { useEffect } from "react";
 import { useState } from "react";
-import { Carousel,Col, Container, ListGroup,  Row } from "react-bootstrap";
+import { Carousel, Col, ListGroup, Row } from "react-bootstrap";
 import { useSelector } from "react-redux";
 
 import productdetail from "../assets/css/productdetail.css";
@@ -19,83 +19,78 @@ const ProductDetail = () => {
   const infoProduct = productList.filter(
     (product) => product.category.id === productDetail.category.id
   );
-  console.log(productList);
-  console.log(productDetail);
+  // console.log(productList);
+  // console.log(productDetail);
+  console.log(infoProduct);
 
   return (
     <Row>
       {/* ===== PRODUCT DETAIL ====== */}
-      <Row lg={12}>
+      <Col >
         <h1 className="product-name">{productDetail?.title}</h1>
 
-        <Row lg={12} className="general-container">
-          {/* ===== OTHER IMAGES PRODUCT DETAIL (small images) ===== */}
-          <Carousel className="carousel-container">
-            {productDetail?.productImgs.map((img) => (
-              <Carousel.Item className="carousel-item" key={img}>
-                <img className="d-block w-100" src={img} alt="First slide" />
-              </Carousel.Item>
-            ))}
-          </Carousel>
-        </Row>
-      </Row>
-          {/* ====== DESCRIPTION ====== 
+        {/* ===== OTHER IMAGES PRODUCT DETAIL (small images) ===== */}
+        <Carousel className="carousel-container">
+          {productDetail?.productImgs.map((img) => (
+            <Carousel.Item className="carousel-item" key={img}>
+              <img className="d-block w-100" src={img} alt="First slide" />
+            </Carousel.Item>
+          ))}
+        </Carousel>
+
+        {/* ====== DESCRIPTION ====== 
           */}
-        <Row>
-          {showDescription ? (
-            <>
-              <button
-                className="show-info-details"
-                onClick={() => setShowDescription(!showDescription)}
-              >
-                {" "}
-                Description
-                <i className="fa-solid fa-chevron-up"></i>
-              </button>
-              <div className="description-container">
-                <p>{infoProduct[0]?.description}</p>
-                <span>
-                  {" "}
-                  {productDetail.status === "active" ? (
-                    <strong>Stock: Available</strong>
-                  ) : (
-                    <strong>Stock: 0</strong>
-                  )}
-                </span>
-              </div>
-            </>
-          ) : (
+        {showDescription ? (
+          <>
             <button
               className="show-info-details"
               onClick={() => setShowDescription(!showDescription)}
             >
-              Description <i className="fa-solid fa-chevron-down"></i>
+              {" "}
+              Description
+              <i className="fa-solid fa-chevron-up"></i>
             </button>
-          )}
-        </Row>
+            <div className="description-container">
+              <p>{infoProduct[0]?.description}</p>
+              <span>
+                {" "}
+                {productDetail.status === "active" ? (
+                  <strong>Stock: Available</strong>
+                ) : (
+                  <strong>Stock: 0</strong>
+                )}
+              </span>
+            </div>
+          </>
+        ) : (
+          <button
+            className="show-info-details"
+            onClick={() => setShowDescription(!showDescription)}
+          >
+            Description <i className="fa-solid fa-chevron-down"></i>
+          </button>
+        )}
+      </Col>
       {/* ====== PRODUCTS RELATED ======
       Col, Container, ListGroup,*/
       }
-      <Row className="products-related-container" lg={12}>
-          <p className="products-related-title">Products related </p>
+      <Col lg={2}>
+        <p>Products related </p>
         <ListGroup>
-          {infoProduct.map((info) => (
-            <Col action key={info.id} lg={3}>
-              <Link
-              className="link"
-                style={{ textDecoration: "none" }}
+          {
+            infoProduct.map((info) => (
+              <ListGroup.Item key={info.id}
                 onClick={() => setChangeImg(info.productImgs?.[0])}
-                to={`/product/${info.id}`}
-              >
-                <div className="container-img">
-                  <img  src={info.productImgs?.[0]} />
+                to={`/product/${info.id}`} >
+
+                <div style={{ backgroundImage: `url(${info.productImgs?.[0]})` }}>
                 </div>
+
                 <p className="products-related-name">{info.title}</p>
-              </Link>
-            </Col>
-          ))}
+              </ListGroup.Item>
+            ))}
         </ListGroup>
-      </Row>
+      </Col>
     </Row>
   );
 };
