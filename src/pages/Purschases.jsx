25a6dbce1210d950810469a16a2c146c8dfba818
tineocac <carlostineocac@gmail.com>
2022-10-06 13:36:43 -0400

    corrections

--- src/pages/Purschases.jsx
+++ src/pages/Purschases.jsx
@@ -1,4 +1,4 @@
-import { isPropertyDescriptor } from "create";
+// import { isPropertyDescriptor } from "create";
 import React, { useEffect } from "react";
 import { Button, Card, CardImg, ListGroup, ListGroupItem } from "react-bootstrap";
 import { useDispatch, useSelector } from "react-redux";
